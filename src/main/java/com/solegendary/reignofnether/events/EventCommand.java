33a9567ad7865fc0e501b3d1196f129e369aa9b5
Shoto <38501963+shoto290@users.noreply.github.com>
2025-09-06 21:13:22 +0200

    fix: only admin can use this command

--- src/main/java/com/solegendary/reignofnether/events/EventCommand.java
+++ src/main/java/com/solegendary/reignofnether/events/EventCommand.java
@@ -9,6 +9,7 @@ import com.solegendary.reignofnether.config.JsonEventConfig;
 import com.solegendary.reignofnether.config.JsonEventConfigManager;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 
 public class EventCommand {
@@ -28,8 +29,20 @@ public class EventCommand {
     }
 
     private static int executeEvent(CommandContext<CommandSourceStack> context, String eventName) throws CommandSyntaxException {
-        ServerPlayer player = context.getSource().getPlayerOrException();
-        EventServerEvents.executeEvent(eventName, player);
-        return Command.SINGLE_SUCCESS;
+        if ((context.getSource() != null &&
+            context.getSource().getPlayer() != null &&
+            context.getSource().getPlayer().hasPermissions(4)) ||
+            (context.getSource() != null &&
+            !context.getSource().isPlayer())) {
+            
+            ServerPlayer player = context.getSource().getPlayerOrException();
+            EventServerEvents.executeEvent(eventName, player);
+            return Command.SINGLE_SUCCESS;
+        } else {
+            if (context.getSource().getPlayer() != null) {
+                context.getSource().getPlayer().sendSystemMessage(Component.literal("You don't have permission to use this command (OP required)"));
+            }
+            return 0;
+        }
     }
 }
